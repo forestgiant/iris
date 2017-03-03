98f3fab9d43eadf42dd1cb41715cfb3daf4bf3de
Stephan Baker <stephan.baker@gmail.com>
2017-03-03 17:21:29 -0500

    Updating cli with getsources/getkeys.

---
+++ cmd/iris-cli/command.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	fglog "github.com/forestgiant/log"
+	"gitlab.fg/otis/iris/api"
+)
+
+type runner struct {
+	Client *api.Client
+	Logger *fglog.Logger
+}
+
+func (r *runner) setValue(source string, key string, value []byte) error {
+	if len(source) == 0 {
+		return errors.New("You must provide a source")
+	}
+
+	if len(key) == 0 {
+		return errors.New("You must provide a key")
+	}
+
+	if len(value) == 0 {
+		return errors.New("You must provide a value")
+	}
+
+	commandCtx, cancelCommand := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancelCommand()
+
+	if err := r.Client.SetValue(commandCtx, source, key, []byte(value)); err != nil {
+		return err
+	}
+
+	r.Logger.Info("Success", "source", source, "key", key, "value", string(value))
+	return nil
+}
+
+func (r *runner) getValue(source, key string) error {
+	if len(source) == 0 {
+		return errors.New("You must provide a source")
+	}
+
+	if len(key) == 0 {
+		return errors.New("You must provide a key")
+	}
+
+	commandCtx, cancelCommand := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancelCommand()
+
+	value, err := r.Client.GetValue(commandCtx, source, key)
+	if err != nil {
+		return err
+	}
+
+	r.Logger.Info("Success", "source", source, "key", key, "value", string(value))
+	return nil
+}
+
+func (r *runner) getSources() error {
+	commandCtx, cancelCommand := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancelCommand()
+
+	sources, err := r.Client.GetSources(commandCtx)
+	if err != nil {
+		return err
+	}
+
+	r.Logger.Info("Success", "count", len(sources))
+	for _, s := range sources {
+		fmt.Println(s)
+	}
+	return nil
+}
+
+func (r *runner) getKeys(source string) error {
+	if len(source) == 0 {
+		return errors.New("You must provide a source")
+	}
+
+	commandCtx, cancelCommand := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancelCommand()
+
+	keys, err := r.Client.GetKeys(commandCtx, source)
+	if err != nil {
+		return err
+	}
+
+	r.Logger.Info("Success", "source", source, "count", len(keys))
+	for _, k := range keys {
+		fmt.Println(k)
+	}
+	return nil
+}
+
+func (r *runner) removeSource(source string) error {
+	if len(source) == 0 {
+		return errors.New("You must provide a source")
+	}
+
+	commandCtx, cancelCommand := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancelCommand()
+
+	if err := r.Client.RemoveSource(commandCtx, source); err != nil {
+		return err
+	}
+
+	r.Logger.Info("Success", "source", source)
+	return nil
+}
+
+func (r *runner) removeValue(source, key string) error {
+	if len(source) == 0 {
+		return errors.New("You must provide a source")
+	}
+
+	if len(key) == 0 {
+		return errors.New("You must provide a key")
+	}
+
+	commandCtx, cancelCommand := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancelCommand()
+
+	if err := r.Client.RemoveValue(commandCtx, source, key); err != nil {
+		return err
+	}
+
+	r.Logger.Info("Success", "source", source, "key", key)
+	return nil
+}
--- cmd/iris-cli/main.go
+++ cmd/iris-cli/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
-	"path/filepath"
 	"time"
 
 	fglog "github.com/forestgiant/log"
@@ -18,70 +18,69 @@ import (
 const (
 	setCommandName          = "set"
 	getCommandName          = "get"
+	getSourcesCommandName   = "getsources"
+	getKeysCommandName      = "getkeys"
 	removeSourceCommandName = "removesource"
-	removeKeyCommandName    = "removekey"
+	removeValueCommandName  = "removekey"
 
-	promptCommand = "prompt for command"
-	promptSource  = "prompt for source"
-	promptKey     = "prompt for key"
-
-	sourceUsage = "The name of the source to be used."
-	sourceParam = "source"
-
-	keyUsage = "The name of the key to be used."
-	keyParam = "key"
-
-	valueUsage = "The value to be used."
-	valueParam = "value"
-
-	addrUsage = "Address of the stela server to connect to."
-	addrParam = "addr"
-
-	caPathUsage = "Path to the certificate authority you would like to use."
-	caPathParam = "ca"
+	sourceUsage   = "The name of the source to be used."
+	sourceParam   = "source"
+	keyUsage      = "The name of the key to be used."
+	keyParam      = "key"
+	valueUsage    = "The value to be used."
+	valueParam    = "value"
+	addrUsage     = "Address of the stela server to connect to."
+	addrParam     = "addr"
+	caPathUsage   = "Path to the certificate authority you would like to use."
+	caPathParam   = "ca"
+	insecureUsage = "Disable SSL, allowing unenecrypted communication with the service."
+	insecureParam = "insecure"
+	noStelaUsage  = "Disable usage of Stela for service discovery."
+	noStelaParam  = "nostela"
 
 	exitStatusSuccess = 0
 	exitStatusError   = 1
 )
+
+func printUsageInstructions() {
+	fmt.Println("usage: iris-cli <command> [<args>]")
+	fmt.Println("The available commands are: ")
+	fmt.Printf("\t%s\t\tSet a value\n", setCommandName)
+	fmt.Printf("\t%s\t\tGet a value\n", getCommandName)
+	fmt.Printf("\t%s\t\tGet a list of available sources\n", getSourcesCommandName)
+	fmt.Printf("\t%s\t\tGet a list of keys contained in a source\n", getKeysCommandName)
+	fmt.Printf("\t%s\tRemove a source\n", removeSourceCommandName)
+	fmt.Printf("\t%s\tRemove a key/value pair\n", removeValueCommandName)
+}
 
 func main() {
 	os.Exit(run())
 }
 
 func run() (status int) {
-
-	// Setup a logger to use
-	var logger = fglog.Logger{}.With("time", fglog.DefaultTimestamp)
-
 	var (
-		ca      string
-		addr    string
-		command string
-		source  string
-		key     string
-		value   string
+		ca       = "ca.cer"
+		addr     string
+		command  string
+		source   string
+		key      string
+		value    string
+		insecure = false
+		noStela  = false
 	)
 
-	// Prepare default path values
-	wd, err := os.Getwd()
-	if err != nil {
-		logger.Error("unable to get current working directory", "error", err)
+	if len(os.Args) <= 1 {
+		printUsageInstructions()
 		return exitStatusError
-	}
-	ca = filepath.Join(wd, "ca.cer")
-
-	if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "-help" {
-
-		if len(os.Args) == 1 {
-			return exitStatusError
-		}
-		return exitStatusSuccess
 	}
 
 	command = os.Args[1]
-
-	if command != setCommandName && command != getCommandName &&
-		command != removeSourceCommandName && command != removeKeyCommandName {
+	if command != setCommandName &&
+		command != getCommandName &&
+		command != getSourcesCommandName &&
+		command != getKeysCommandName &&
+		command != removeSourceCommandName &&
+		command != removeValueCommandName {
 		printUsageInstructions()
 		return exitStatusError
 	}
@@ -92,83 +91,73 @@ func run() (status int) {
 	flag.StringVar(&source, sourceParam, source, sourceUsage)
 	flag.StringVar(&key, keyParam, key, keyUsage)
 	flag.StringVar(&value, valueParam, value, valueUsage)
+	flag.BoolVar(&insecure, insecureParam, insecure, insecureUsage)
+	flag.BoolVar(&noStela, noStelaParam, noStela, noStelaUsage)
 	flag.Parse(os.Args[2:])
 
-	if len(addr) == 0 {
-		ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
-		defer cancel()
-		stelaclient, err := stela_api.NewClient(ctx, stela.DefaultStelaAddress, "")
-		if err != nil {
-			logger.Error("Failed to obtain stela client.", "error", err.Error())
-			return exitStatusError
-		}
-
-		discoverCtx, cancelDiscover := context.WithTimeout(context.Background(), 200*time.Millisecond)
-		defer cancelDiscover()
-		service, err := stelaclient.DiscoverOne(discoverCtx, iris.DefaultServiceName)
-		if err != nil {
-			logger.Error("Failed to discover iris server.", "error", err.Error())
-			return exitStatusError
-		}
-		addr = service.IPv4Address()
+	if insecure {
+		ca = ""
 	}
 
+	if len(addr) == 0 && !noStela {
+		ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+		defer cancel()
+		stelaclient, err := stela_api.NewClient(ctx, stela.DefaultStelaAddress, ca)
+		if err == nil {
+			defer stelaclient.Close()
+
+			discoverCtx, cancelDiscover := context.WithTimeout(context.Background(), 200*time.Millisecond)
+			defer cancelDiscover()
+			service, err := stelaclient.DiscoverOne(discoverCtx, iris.DefaultServiceName)
+			if err == nil {
+				addr = service.IPv4Address()
+			}
+		}
+	}
+
+	if len(addr) == 0 {
+		addr = fmt.Sprintf("127.0.0.1:%d", iris.DefaultServicePort)
+	}
+	fmt.Printf("Connecting to %s.\n", addr)
+
 	var client *api.Client
+	var err error
 	connectCtx, cancelConnect := context.WithTimeout(context.Background(), 200*time.Millisecond)
 	defer cancelConnect()
-	if len(ca) == 0 {
+	if insecure || len(ca) == 0 {
 		client, err = api.NewClient(connectCtx, addr, nil)
 	} else {
 		client, err = api.NewTLSClient(connectCtx, addr, "iris.forestgiant.com", ca)
 	}
 
 	if err != nil {
-		logger.Error("Failed to connect to iris server.", "error", err.Error())
+		fmt.Println("Failed to connect to Iris server.", err)
 		return exitStatusError
 	}
+	defer client.Close()
 
-	commandCtx, cancelCommand := context.WithTimeout(context.Background(), 500*time.Millisecond)
-	defer cancelCommand()
-
+	r := &runner{Client: client, Logger: &fglog.Logger{Formatter: fglog.LogfmtFormatter{}}}
 	switch command {
 	case setCommandName:
-		if err := client.SetValue(commandCtx, source, key, []byte(value)); err != nil {
-			logger.Error("Failed to set value.", "error", err.Error())
-			return exitStatusError
-		}
-		logger.Info("Success", "command", command, "source", source, "key", key, "value", value)
+		err = r.setValue(source, key, []byte(value))
 	case getCommandName:
-		value, err := client.GetValue(commandCtx, source, key)
-		if err != nil {
-			logger.Error("Failed to get value.", "error", err.Error())
-			return exitStatusError
-		}
-		logger.Info("Success", "command", command, "source", source, "key", key, "value", string(value))
+		err = r.getValue(source, key)
+	case getSourcesCommandName:
+		err = r.getSources()
+	case getKeysCommandName:
+		err = r.getKeys(source)
 	case removeSourceCommandName:
-		if err := client.RemoveSource(commandCtx, source); err != nil {
-			logger.Error("Failed to remove source.", "error", err.Error())
-			return exitStatusError
-		}
-		logger.Info("Success", "command", command, "source", source, "key")
-	case removeKeyCommandName:
-		if err := client.RemoveValue(commandCtx, source, key); err != nil {
-			logger.Error("Failed to remove source/key/value.", "error", err.Error())
-			return exitStatusError
-		}
-		logger.Info("Success", "command", command, "source", source, "key", key)
+		err = r.removeSource(source)
+	case removeValueCommandName:
+		err = r.removeValue(source, key)
 	default:
-		logger.Error("Unknown command")
+		err = errors.New("Unknown command")
+	}
+
+	if err != nil {
+		fmt.Println(err)
 		return exitStatusError
 	}
 
 	return exitStatusSuccess
-}
-
-func printUsageInstructions() {
-	fmt.Println("usage: iris-cli <command> [<args>]")
-	fmt.Println("The available commands are: ")
-	fmt.Printf("\t%s\t\tSet a value\n", setCommandName)
-	fmt.Printf("\t%s\t\tGet a value\n", getCommandName)
-	fmt.Printf("\t%s\tRemove a source\n", removeSourceCommandName)
-	fmt.Printf("\t%s\tRemove a key/value pair\n", removeKeyCommandName)
 }
